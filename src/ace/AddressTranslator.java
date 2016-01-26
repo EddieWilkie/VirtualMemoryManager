628bd4fbcd597c5ba9c1319bedca6570d72d073e
EddieWilkie <edward.wilkie.2013@uni.strath.ac.uk>
2016-01-26 12:53:46 +0000

    trying out the new TLB. Still getting NullPointerException

--- src/ace/AddressTranslator.java
+++ src/ace/AddressTranslator.java
@@ -11,13 +11,12 @@ public class AddressTranslator {
 	BufferedReader br;
 	String address = "";
 	int pageNumber, offset;
-	IPageTable pageTable;
+	TLB tlb;
 	
 
 	public AddressTranslator() throws FileNotFoundException {
 		fr = new FileReader("InputFile.txt");
 		br = new BufferedReader(fr);
-		pageTable = new PageTable();
 	}
 
 	public void begin() throws IOException {
@@ -26,10 +25,11 @@ public class AddressTranslator {
 			pageNumber = (i >> 8);
 			offset = (i & 0xFF);
 			System.out.print("Virtual address: " + i + " ");
-			System.out.print("Physical Address: " + ((pageTable.getFrameNumber(pageNumber) << 8) + offset) + " ");
-			System.out.println("Value: " + pageTable.getValue(pageNumber, offset));
+			System.out.println("Value: " + tlb.getValue(pageNumber, offset));
+			//System.out.print("Physical Address: " + ((pageTable.getFrameNumber(pageNumber) << 8) + offset) + " ");
+			//System.out.println("Value: " + pageTable.getValue(pageNumber, offset));
 		}
-			System.out.println("PageFaultPercent: " + (pageTable.getPageFaultCount()/256)*100+ "%");
+			//System.out.println("PageFaultPercent: " + (pageTable.getPageFaultCount()/256)*100+ "%");
 	}
 
 	public static void main(String[] args) {
--- src/ace/TLB.java
+++ src/ace/TLB.java
@@ -1,7 +1,48 @@
 package ace;
 
+import java.util.Hashtable;
+import java.util.Random;
+
 public class TLB implements ITLB{
 
+	private int[] pageNumber, frameNumber;
+	private IPhysicalMemory physicalMemory;
+	private IPageTable pageTable;
+	private int currentPageNumber = 0;
 	
+	public TLB(){
+	initArrays();
+	this.pageNumber = new int[16];
+	this.frameNumber = new int[16];
+	pageTable = new PageTable(this,physicalMemory);
+	}
+	
+	private void initArrays(){
+		for(int i = 0; i < pageNumber.length; i++){
+			pageNumber[i] = -1;
+			frameNumber[i] = -1;
+		}
+	}
 
+	public void setFrameNumber(int frameNumber){
+		Random r = new Random();
+		int Low = 0;
+		int High = 15;
+		int Result = r.nextInt(High-Low) + Low;
+		this.pageNumber[Result] = currentPageNumber;
+		this.frameNumber[Result] = frameNumber;
+	}
+	
+	public int getValue(int pageNum, int offset){
+		this.currentPageNumber = pageNum;
+		System.out.println("test");
+//		for(int i = 0; i < this.pageNumber.length; i++){
+//			if(this.pageNumber[i] == pageNum){
+//				System.out.println(this.pageNumber[i]);
+//				return physicalMemory.getFrameValue(this.frameNumber[i],offset);
+//			}
+//		}
+		return 1;
+		//return pageTable.getValue(pageNum, offset);
+	}
 }
